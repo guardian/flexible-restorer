00bca2e57394b7d86d0760110e4327570e655a2d
Jon Parsons <jon.parsons@guardian.co.uk>
2015-03-25 14:53:56 +0000

    fixed routing for restorer list view

--- public/config.js
+++ public/config.js
@@ -8,14 +8,20 @@ System.config({
   },
   "paths": {
     "*": "*.js",
-    "npm:*": "jspm_packages/npm/*.js"
+    "npm:*": "jspm_packages/npm/*.js",
+    "github:*": "jspm_packages/github/*.js"
   }
 });
 
 System.config({
   "map": {
+    "angular": "github:angular/bower-angular@1.3.15",
+    "angular-route": "github:angular/bower-angular-route@1.3.15",
     "babel": "npm:babel@4.7.16",
-    "babel-runtime": "npm:babel-runtime@4.7.16"
+    "babel-runtime": "npm:babel-runtime@4.7.16",
+    "github:angular/bower-angular-route@1.3.15": {
+      "angular": "github:angular/bower-angular@1.3.15"
+    }
   }
 });
 
