1b1ed5b3a2a0befba8d5c37c6101b36b08fb7800
Jon Parsons <jon.parsons@guardian.co.uk>
2015-03-25 13:37:52 +0000

    added jspm to project

---
+++ public/config.js
@@ -0,0 +1,21 @@
+System.config({
+  "baseURL": "assets",
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
+  "paths": {
+    "*": "*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "babel": "npm:babel@4.7.16",
+    "babel-runtime": "npm:babel-runtime@4.7.16"
+  }
+});
+
