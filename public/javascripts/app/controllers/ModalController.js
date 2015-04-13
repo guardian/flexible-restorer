902ca07b81e003b22a6d3599f0660d0584c9fd2e
Jon Parsons <jon.parsons@guardian.co.uk>
2015-04-13 11:40:55 +0100

    added body locking whilst modal is open

--- public/javascripts/app/controllers/ModalController.js
+++ public/javascripts/app/controllers/ModalController.js
@@ -20,19 +20,24 @@ var ModalCtrl = ModalCtrlMod.controller('ModalCtrl', [
     var showModal = this.showModal = function showModal(){
       $scope.$apply(() => {
         $scope.isActive = true;
+        //LOCK THE BODY ELEMENT SO A USER CANNOT
+        //SCROLL DOWN THE PAGE WHILST THE MODAL IS OPEN
+        angular.element(document.body).css({ overflow: 'hidden' });
       });
-    }
+    };
 
     this.clickCloseModal = function clickCloseModal(){
       $timeout(closeModal, 1);
-    }
+    };
 
     var closeModal = this.closeModal = function closeModal(){
       $scope.$apply(() => {
         $scope.isActive = false;
+        // UNLOCK THE BODY ELEMENT
+        angular.element(document.body).css({ height: '100%', overflow: 'visible' });
         mediator.publish('snapshot-list:hidden-modal', showModal);
       });
-    }
+    };
 
     mediator.subscribe('snapshot-list:display-modal', showModal);
     mediator.subscribe('snapshot-list:close-modal',   closeModal);
