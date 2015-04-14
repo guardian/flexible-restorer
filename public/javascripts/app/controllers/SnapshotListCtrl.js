a8ec8308dd90ff85970de004e9fa5c7830253b5d
Jon Parsons <jon.parsons@guardian.co.uk>
2015-04-14 10:04:53 +0100

    fixed typo

--- public/javascripts/app/controllers/SnapshotListCtrl.js
+++ public/javascripts/app/controllers/SnapshotListCtrl.js
@@ -4,7 +4,7 @@ import mediator from '../utils/mediator';
 
 var SnapshotListCtrlMod = angular.module('SnapshotListCtrlMod', []);
 
-var SnapshotListCtrl = restoreListCtrlMod.controller('SnapshotListCtrl', [
+var SnapshotListCtrl = SnapshotListCtrlMod.controller('SnapshotListCtrl', [
   '$scope',
   '$routeParams',
   '$timeout',
