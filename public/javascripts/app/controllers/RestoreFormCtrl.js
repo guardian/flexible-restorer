d9e383ffd61e7cd6f1e7791d266b12f05825e620
Jon Parsons <jon.parsons@guardian.co.uk>
2015-04-16 15:26:32 +0100

    added re-direct to composer

--- public/javascripts/app/controllers/RestoreFormCtrl.js
+++ public/javascripts/app/controllers/RestoreFormCtrl.js
@@ -15,14 +15,15 @@ RestoreFormCtrlMod.controller('RestoreFormCtrl', [
       //TODO ADD POST JP 13/4/15
       $scope.isLoading = true;
       RestoreService
-        .restore()
-        .then((data) => {
-          //redirect back to composer
-          console.log('-----------------------');
-          console.log('This has worked', data);
-          console.log('-----------------------');
-        })
-        .catch((err) => mediator.publish('error', err));
+      .restore()
+      .then((data) => {
+        //redirect back to composer
+        var env = window.location.origin.split('.')[1];
+        var url = (env === 'gutools') ? 'https://composer.gutools.co.uk' : `https://composer.${env}.dev-gutools.co.uk`;
+        window.location.href = `${url}/${contentId}`;
+
+      })
+      .catch((err) => mediator.publish('error', err));
     };
 
     mediator.subscribe('snapshot-list:hidden-modal', resetModalForm);
