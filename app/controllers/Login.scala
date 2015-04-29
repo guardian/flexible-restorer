8febddd68ebe1da821061dee477d38c18d5a8166
CutAndPastey <jon.parsons@guardian.co.uk>
2015-04-29 15:33:33 +0100

    added user api endpoint

--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -3,6 +3,7 @@ package controllers
 import play.api.mvc._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.json._
 
 object Login extends Controller with PanDomainAuthActions {
 
@@ -17,4 +18,9 @@ object Login extends Controller with PanDomainAuthActions {
   def authError(message: String) = Action.async { implicit request =>
     Future(Forbidden(views.html.Login.authError(message)))
   }
+
+  def user() = AuthAction { implicit request =>
+    Ok(request.user.toJson).as(JSON)
+  }
+
 }
