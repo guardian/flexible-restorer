2093e07dfa7ae1cc4f7f693cef4754136eb21c71
Susie Coleman <susie.coleman@guardian.co.uk>
2016-05-12 14:47:19 +0100

    Add logging.

--- public/javascripts/app/controllers/ErrorCtrl.js
+++ public/javascripts/app/controllers/ErrorCtrl.js
@@ -4,18 +4,20 @@ import mediator from '../utils/mediator';
 var ErrorCtrlMod = angular.module('ErrorCtrlMod', []);
 
 ErrorCtrlMod.controller('ErrorCtrl', [
-  '$element',
-  function($element){
-    $element.attr('style', '');
-    this.hasError     = false;
-    this.errorContent = '';
+    '$element',
+    '$log',
+    function ($element, $log) {
+        $element.attr('style', '');
+        this.hasError = false;
+        this.errorContent = '';
 
-    mediator.subscribe('error', (err)=> {
-      //TODO JP 15/4/15 ADD CS LOGGING HERE
-      this.hasError = true;
-      this.errorContent = err.message;
-    });
-  }
+        mediator.subscribe('error', (err) => {
+            //TODO JP 15/4/15 ADD CS LOGGING HERE
+            this.hasError = true;
+            this.errorContent = err.message;
+            $log.error(err.message);
+        });
+    }
 ]);
 
 export default ErrorCtrlMod;
