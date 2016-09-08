be542e60b83dba3ee4edbd87c40d739c18e339ae
Simon Hildrew <simon.hildrew@theguardian.com>
2016-09-08 13:53:00 +0100

    Parse API gateway input

--- app/LambdaEntrypoint.scala
+++ app/LambdaEntrypoint.scala
@@ -1,12 +1,12 @@
-import java.io.File
-import java.util
-import java.util.{Map => JMap}
+import java.io.{File, InputStream, OutputStream}
+import java.util.{LinkedHashMap => JavaLinkedHashMap}
 
 import akka.actor.Cancellable
 import akka.stream.{ClosedShape, Graph, Materializer}
 import com.amazonaws.services.lambda.runtime.{LambdaLogger, Context => λContext}
 import play.api.ApplicationLoader.Context
-import play.api.mvc.{AnyContentAsEmpty, RequestHeader}
+import play.api.libs.json.Json
+import play.api.mvc.AnyContentAsEmpty
 import play.api.test.{FakeHeaders, FakeRequest, Helpers, Writeables}
 import play.api.{Configuration, Environment, Mode, Play}
 import play.core.DefaultWebCommands
@@ -15,8 +15,7 @@ import scala.collection.JavaConverters._
 import scala.concurrent.Await
 import scala.concurrent.duration.FiniteDuration
 
-class LambdaEntrypoint extends Writeables {
-
+object Loader {
   println("We started and ran!")
   val appLoader = new AppLoader()
   println("We newed up an app loader")
@@ -33,33 +32,35 @@ class LambdaEntrypoint extends Writeables {
 
   Play.start(app)
   println("Play application started")
-
-  def run(event: JMap[String, Object], context: λContext): Unit = {
-    implicit val logger = context.getLogger
-    val request = Request.fromEvent(event.asScala.toMap)
-
-    // actually call the router
-    val maybeResult = Helpers.route(app, request)(Helpers.writeableOf_AnyContentAsEmpty)
-    context.getLogger.log(maybeResult.fold(s"Route not found for $request")(_ => s"Successfully routed $request"))
-
-    import Helpers.defaultAwaitTimeout
-
-    maybeResult.foreach { of =>
-      val body = Helpers.contentAsString(of)
-      val result = Await.result(of, Helpers.defaultAwaitTimeout.duration)
-      context.getLogger.log(s"Result:\n${result.header.status} ${result.header.reasonPhrase}\n$body")
-    }
-
-    context.getLogger.log("Finished")
-  }
 }
 
+object Params { implicit val reads = Json.reads[Params] }
+case class Params(header: Map[String, String])
+
 object Request {
+  implicit val reads = Json.reads[Request]
+
+  // TODO: this should really return an either with better error reporting
+  def fromStream(stream: InputStream)(implicit logger: LambdaLogger): Option[FakeRequest[AnyContentAsEmpty.type]] = {
+    val json = Json.parse(stream)
+    logger.log(s"Got JSON: $json")
+    val request = Json.fromJson[Request](json)
+    logger.log(s"Parsed request: $request")
+    request.map { r =>
+      FakeRequest(
+        method = r.method,
+        uri = r.uri,
+        headers = FakeHeaders(r.params.header.toList),
+        body = AnyContentAsEmpty
+      )
+    }.asOpt
+  }
+
   def fromEvent(event: Map[String, Object])(implicit logger: LambdaLogger): FakeRequest[AnyContentAsEmpty.type] = {
     val method = event("method").asInstanceOf[String]
     val uri = event("uri").asInstanceOf[String]
     val headers = event.get("headers").map {
-      case headerMap: util.LinkedHashMap[_,_] =>
+      case headerMap: JavaLinkedHashMap[_,_] =>
         headerMap.entrySet.asScala.flatMap { entry =>
           (entry.getKey, entry.getValue) match {
             case (key: String, value: String) => Some(key, value)
@@ -77,6 +78,39 @@ object Request {
       headers = FakeHeaders(headers.toList),
       body = AnyContentAsEmpty
     )
+  }
+}
+
+case class Request(method: String, uri: String, params: Params)
+
+// TODO: body should really be a byte array
+object Response {
+  implicit val writes = Json.writes[Response]
+}
+case class Response(body: String, status: Int, reason: Option[String], headers: Map[String, String])
+
+class LambdaEntrypoint extends Writeables {
+  def run(lambdaRequest: InputStream, lambdaResponse: OutputStream, context: λContext): Unit = {
+    implicit val logger = context.getLogger
+    val request = Request.fromStream(lambdaRequest)
+
+    // actually call the router
+    val maybeResult = request.flatMap(Helpers.route(Loader.app, _)(Helpers.writeableOf_AnyContentAsEmpty))
+    logger.log(maybeResult.fold(s"Route not found for $request")(_ => s"Successfully routed $request"))
+
+    import Helpers.defaultAwaitTimeout
+
+    maybeResult.foreach { of =>
+      val body = Helpers.contentAsString(of)
+      val result = Await.result(of, Helpers.defaultAwaitTimeout.duration)
+      val response = Response(body = body, status = result.header.status, result.header.reasonPhrase, result.header.headers)
+      logger.log(s"Response object:\n$response")
+      val json = Json.toJson(response)
+      lambdaResponse.write(json.toString.getBytes("UTF-8"))
+      logger.log(s"Written JSON to response stream")
+    }
+
+    context.getLogger.log("Finished")
   }
 }
 
