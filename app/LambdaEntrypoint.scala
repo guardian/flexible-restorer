9633cc908fd9265d462ad1954227ac7d6b08ef9e
Simon Hildrew <simon.hildrew@theguardian.com>
2016-09-27 17:49:12 +0100

    Pre-proxy lambda checkin

--- app/LambdaEntrypoint.scala
+++ app/LambdaEntrypoint.scala
@@ -1,21 +1,24 @@
 import java.io.{File, InputStream, OutputStream}
 import java.util.{LinkedHashMap => JavaLinkedHashMap}
 
-import akka.actor.Cancellable
-import akka.stream.{ClosedShape, Graph, Materializer}
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 import com.amazonaws.services.lambda.runtime.{LambdaLogger, Context => λContext}
+import com.amazonaws.util.BinaryUtils
 import play.api.ApplicationLoader.Context
 import play.api.libs.json.Json
-import play.api.mvc.AnyContentAsEmpty
+import play.api.mvc.{AnyContentAsEmpty, Result}
 import play.api.test.{FakeHeaders, FakeRequest, Helpers, Writeables}
 import play.api.{Configuration, Environment, Mode, Play}
 import play.core.DefaultWebCommands
 
 import scala.collection.JavaConverters._
 import scala.concurrent.Await
-import scala.concurrent.duration.FiniteDuration
+import scala.language.postfixOps
 
-object Loader {
+object LambdaEntrypoint {
+  implicit val system = ActorSystem("LambdaActorSystem")
+  implicit val materializer = ActorMaterializer()
   println("We started and ran!")
   val appLoader = new AppLoader()
   println("We newed up an app loader")
@@ -35,10 +38,14 @@ object Loader {
 }
 
 object Params { implicit val reads = Json.reads[Params] }
-case class Params(header: Map[String, String])
+case class Params(header: Map[String, String], path: Map[String, String])
 
 object Request {
   implicit val reads = Json.reads[Request]
+
+  def buildPath(uri: String, pathParams: Map[String, String]): String = {
+    pathParams.foldLeft(uri){ case (acc, (key, value)) => acc.replace(s"{$key}", value) }
+  }
 
   // TODO: this should really return an either with better error reporting
   def fromStream(stream: InputStream)(implicit logger: LambdaLogger): Option[FakeRequest[AnyContentAsEmpty.type]] = {
@@ -47,9 +54,10 @@ object Request {
     val request = Json.fromJson[Request](json)
     logger.log(s"Parsed request: $request")
     request.map { r =>
+      val substitutedPath = buildPath(r.uri, r.params.path)
       FakeRequest(
         method = r.method,
-        uri = r.uri,
+        uri = substitutedPath,
         headers = FakeHeaders(r.params.header.toList),
         body = AnyContentAsEmpty
       )
@@ -90,37 +98,41 @@ object Response {
 case class Response(body: String, status: Int, reason: Option[String], headers: Map[String, String])
 
 class LambdaEntrypoint extends Writeables {
+  import LambdaEntrypoint.materializer
+
   def run(lambdaRequest: InputStream, lambdaResponse: OutputStream, context: λContext): Unit = {
     implicit val logger = context.getLogger
     val request = Request.fromStream(lambdaRequest)
 
     // actually call the router
-    val maybeResult = request.flatMap(Helpers.route(Loader.app, _)(Helpers.writeableOf_AnyContentAsEmpty))
+    val maybeResult = request.flatMap(Helpers.route(LambdaEntrypoint.app, _)(Helpers.writeableOf_AnyContentAsEmpty))
     logger.log(maybeResult.fold(s"Route not found for $request")(_ => s"Successfully routed $request"))
 
     import Helpers.defaultAwaitTimeout
 
     maybeResult.foreach { of =>
-      val body = Helpers.contentAsString(of)
-      val result = Await.result(of, Helpers.defaultAwaitTimeout.duration)
-      val response = Response(body = body, status = result.header.status, result.header.reasonPhrase, result.header.headers)
+      val body = BinaryUtils.toBase64(Helpers.contentAsBytes(of).toArray)
+      val result: Result = Await.result(of, Helpers.defaultAwaitTimeout.duration)
+      val headers = result.header.headers ++
+        result.body.contentType.map("Content-Type" ->) ++
+        result.body.contentLength.map("Content-Length" -> _.toString)
+      val response = Response(body = body, status = result.header.status, result.header.reasonPhrase, headers)
       logger.log(s"Response object:\n$response")
       val json = Json.toJson(response)
       lambdaResponse.write(json.toString.getBytes("UTF-8"))
+      lambdaResponse.flush()
       logger.log(s"Written JSON to response stream")
+      if (response.status != 200) {
+        throw new LocationException(s"status=${response.status}", response.headers("Location"))
+      }
     }
 
     context.getLogger.log("Finished")
   }
 }
 
-object NoMaterializer extends Materializer {
-  def withNamePrefix(name: String) = throw new UnsupportedOperationException("NoMaterializer cannot be named")
-  implicit def executionContext = throw new UnsupportedOperationException("NoMaterializer does not have an execution context")
-  def materialize[Mat](runnable: Graph[ClosedShape, Mat]) =
-    throw new UnsupportedOperationException("No materializer was provided, probably when attempting to extract a response body, but that body is a streamed body and so requires a materializer to extract it.")
-  override def scheduleOnce(delay: FiniteDuration, task: Runnable): Cancellable =
-    throw new UnsupportedOperationException("NoMaterializer can't schedule tasks")
-  override def schedulePeriodically(initialDelay: FiniteDuration, interval: FiniteDuration, task: Runnable): Cancellable =
-    throw new UnsupportedOperationException("NoMaterializer can't schedule tasks")
+class LocationException(errorMessage: String, location: String) extends Exception(errorMessage) {
+  val locationQuery = if (location.contains('?')) "&ignoreThisQueryParameter=" else "?ignoreThisQueryParameter="
+  val locationElement = new StackTraceElement(s"$location$locationQuery", "", "", 0)
+  override def getStackTrace: Array[StackTraceElement] = Seq(locationElement).toArray
 }
