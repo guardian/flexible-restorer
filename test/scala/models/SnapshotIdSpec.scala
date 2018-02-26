b9888fd06c07c8fc1bd0124bf757bd439cc5e8fa
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2018-02-26 12:55:24 +0000

    Update scalatest

--- test/scala/models/SnapshotIdSpec.scala
+++ test/scala/models/SnapshotIdSpec.scala
@@ -1,9 +1,10 @@
 package scala.models
 
 import models.SnapshotId
-import org.scalatest.{FlatSpec, ShouldMatchers}
+import org.scalatest.{FlatSpec, Matchers}
+import Matchers._
 
-class SnapshotIdSpec extends FlatSpec with ShouldMatchers {
+class SnapshotIdSpec extends FlatSpec {
   "fromKey" should "extract the ID and timestamp from a key" in {
     val key = "575ee43ee4b0625eba5110f6/2016-06-13T16:57:50.306Z.json"
     SnapshotId.fromKey(key) should be(Some(SnapshotId("575ee43ee4b0625eba5110f6", "2016-06-13T16:57:50.306Z")))
