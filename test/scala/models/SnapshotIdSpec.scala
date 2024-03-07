e6763bfdc78773c80adac8cec12fa7507ebc0fa7
David Furey <david.furey@guardian.co.uk>
2024-03-07 09:55:48 +0000

    Fix Scalatest imports

--- test/scala/models/SnapshotIdSpec.scala
+++ test/scala/models/SnapshotIdSpec.scala
@@ -1,10 +1,10 @@
 package scala.models
 
 import models.SnapshotId
-import org.scalatest.{FlatSpec, Matchers}
-import Matchers._
+import org.scalatest.flatspec._
+import org.scalatest.matchers.should.Matchers._
 
-class SnapshotIdSpec extends FlatSpec {
+class SnapshotIdSpec extends AnyFlatSpec {
   "fromKey" should "extract the ID and timestamp from a key" in {
     val key = "575ee43ee4b0625eba5110f6/2016-06-13T16:57:50.306Z.json"
     SnapshotId.fromKey(key) should be(Some(SnapshotId("575ee43ee4b0625eba5110f6", "2016-06-13T16:57:50.306Z")))
