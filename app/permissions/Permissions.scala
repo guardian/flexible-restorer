edbe2aa904fd3327ee70f93687bc195aa7f717a7
David Furey <david.furey@guardian.co.uk>
2024-03-07 09:02:45 +0000

    Changes to support permissions client v2.15
    Permissions are now available synchronously which simplifies things quite a bit

--- app/permissions/Permissions.scala
+++ app/permissions/Permissions.scala
@@ -1,43 +1,13 @@
 package permissions
 
-import com.amazonaws.auth.AWSCredentialsProvider
-import com.gu.editorial.permissions.client._
-import com.gu.pandomainauth.model.User
-import config.AWS._
+import com.gu.permissions.PermissionDefinition
 
-import scala.concurrent.Future
-import scala.language.postfixOps
-
-/**
- * Adapter for the Permissions client library
- */
-class Permissions(stage: String, awsCredentials: AWSCredentialsProvider = credentialsV1) extends PermissionsProvider {
+object Permissions {
   val app = "composer-restorer"
 
-  implicit def config: PermissionsConfig = {
-    val s3BucketPrefix = if (stage == "PROD") "PROD" else "CODE"
+  val RestoreContent: PermissionDefinition = PermissionDefinition("restore_content", app)
 
-    PermissionsConfig(
-      app = app,
-      all = all,
-      s3BucketPrefix = s3BucketPrefix,
-      awsCredentials = awsCredentials
-    )
-  }
+  val RestoreContentToAlternateStack: PermissionDefinition = PermissionDefinition("restore_content_to_any_stack", app)
 
-  val RestoreContent: Permission = Permission("restore_content", app, PermissionDenied)
-
-  val RestoreContentToAlternateStack: Permission = Permission("restore_content_to_any_stack", app, PermissionDenied)
-
-  val all: Seq[Permission] = Seq(RestoreContent, RestoreContentToAlternateStack)
-
-  def isGranted(user: User, permission: Permission): Future[Boolean] = {
-    get(permission)(PermissionsUser(user.email)).map {
-      case PermissionGranted => true
-      case _ => false
-    }
-  }
-
-  def userPermissionMap(user: User): Future[Map[Permission, Boolean]] =
-    Future.sequence(all.map(p => isGranted(user, p).map(p ->))).map(_.toMap)
+  val all: Seq[PermissionDefinition] = Seq(RestoreContent, RestoreContentToAlternateStack)
 }
