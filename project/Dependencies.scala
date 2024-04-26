bb357c62a905c5d7b841beb0d318293f1e941f01
David Furey <david.furey@guardian.co.uk>
2024-04-26 18:12:54 +0100

    Bump lots of dependencies
    Keep Snyk happy

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import play.sbt.PlayImport.ws
 import sbt._
 
 object Dependencies {
-  val awsVersion = "2.23.13"
-  val awsVersionV1 = "1.12.470"
+  val awsVersion = "2.25.27"
+  val awsVersionV1 = "1.12.584"
 
   val awsDependencies = Seq(
     "com.amazonaws" % "aws-java-sdk-s3" % awsVersionV1,
@@ -14,24 +14,24 @@ object Dependencies {
   )
 
   val guardianDependencies = Seq(
-    "com.gu" %% "pan-domain-auth-play_3-0" % "3.0.1",
+    "com.gu" %% "pan-domain-auth-play_3-0" % "3.0.3",
     "com.gu" %% "editorial-permissions-client" % "2.15",
-    "com.gu" %% "simple-configuration-ssm" % "1.6.4"
+    "com.gu" %% "simple-configuration-ssm" % "2.0.0"
   )
 
   val testDependencies = Seq(
-    "org.scalatest" %% "scalatest" % "3.2.17" % Test
+    "org.scalatest" %% "scalatest" % "3.2.18" % Test
   )
 
   val dependencies = Seq(
     ws,
-    "org.playframework" %% "play-json-joda" % "3.0.1",
-    "net.logstash.logback" % "logstash-logback-encoder" % "7.3",
+    "org.playframework" %% "play-json-joda" % "3.0.2",
+    "net.logstash.logback" % "logstash-logback-encoder" % "7.4",
     "org.eclipse.jgit" % "org.eclipse.jgit" % "5.1.1.201809181055-r",
-    "commons-io" % "commons-io" % "2.15.1",
-    "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % "2.12.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.14.3",
-    "org.jsoup" % "jsoup" % "1.11.3",
-    "com.lihaoyi" %% "ujson" % "3.1.4",
+    "commons-io" % "commons-io" % "2.16.1",
+    "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % "2.17.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.17.0",
+    "org.jsoup" % "jsoup" % "1.17.2",
+    "com.lihaoyi" %% "ujson" % "3.3.0",
   ) ++ awsDependencies ++ guardianDependencies ++ testDependencies
 }
