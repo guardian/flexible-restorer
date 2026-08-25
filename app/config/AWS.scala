558da4e83011cfe6eec53cf4dc565eb591594cc2
Dominic Kendrick <dominic.kendrick@theguardian.com>
2026-08-25 13:18:15 +0000

    Support EnvironmentVariableCredentialsProvider to simplfy aws credentials management in testing

--- app/config/AWS.scala
+++ app/config/AWS.scala
@@ -1,6 +1,11 @@
 package config
 
-import software.amazon.awssdk.auth.credentials.{AwsCredentialsProviderChain, InstanceProfileCredentialsProvider, ProfileCredentialsProvider}
+import software.amazon.awssdk.auth.credentials.{
+  AwsCredentialsProviderChain,
+  EnvironmentVariableCredentialsProvider,
+  InstanceProfileCredentialsProvider,
+  ProfileCredentialsProvider
+}
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
 
@@ -11,10 +16,18 @@ object AWS {
   lazy val defaultAppName = "restorer"
   lazy val defaultRegion: Region = Region.EU_WEST_1
 
-  val credentials: AwsCredentialsProviderChain = AwsCredentialsProviderChain.builder().credentialsProviders(
-    ProfileCredentialsProvider.create(profile),
-    InstanceProfileCredentialsProvider.create()
-  ).build()
+  val credentials: AwsCredentialsProviderChain = AwsCredentialsProviderChain
+    .builder()
+    .credentialsProviders(
+      EnvironmentVariableCredentialsProvider.create(),
+      ProfileCredentialsProvider.create(profile),
+      InstanceProfileCredentialsProvider.create()
+    )
+    .build()
 
-  lazy val s3Client: S3Client = S3Client.builder().credentialsProvider(credentials).region(defaultRegion).build()
+  lazy val s3Client: S3Client = S3Client
+    .builder()
+    .credentialsProvider(credentials)
+    .region(defaultRegion)
+    .build()
 }
