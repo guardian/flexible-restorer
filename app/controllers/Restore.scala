b58da96dd1871bac48265dbc31e83efa184555a6
Dominic Kendrick <dominic.kendrick@theguardian.com>
2026-09-01 09:30:00 +0000

    Remove fomatting

--- app/controllers/Restore.scala
+++ app/controllers/Restore.scala
@@ -19,31 +19,59 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.util.control.NonFatal
 
 class Restore(
-  val controllerComponents: ControllerComponents,
-  snapshotApi: SnapshotApi,
-  flexibleApi: FlexibleApi,
-  val config: AppConfig,
-  val wsClient: WSClient,
-  override val permissions: PermissionsProvider,
-  override val panDomainSettings: PanDomainAuthSettingsRefresher
-) extends BaseController with PanDomainAuthActions with Loggable {
+    val controllerComponents: ControllerComponents,
+    snapshotApi: SnapshotApi,
+    flexibleApi: FlexibleApi,
+    val config: AppConfig,
+    val wsClient: WSClient,
+    override val permissions: PermissionsProvider,
+    override val panDomainSettings: PanDomainAuthSettingsRefresher
+) extends BaseController
+    with PanDomainAuthActions
+    with Loggable {
 
-  def userFromPandaUser(user: PandaUser) = User(user.firstName, user.lastName, user.email)
+  def userFromPandaUser(user: PandaUser) =
+    User(user.firstName, user.lastName, user.email)
 
-  def restore(sourceId: String, contentId: String, timestamp: String, destinationId: String) = AuthAction.async { request =>
-    if (!permissions.hasPermission(Permissions.RestoreContent, request.user.email)) {
-      Future.successful(Forbidden(s"You do not have the ${Permissions.RestoreContent.name} permission which is required to restore content"))
-    } else if (sourceId != destinationId && !permissions.hasPermission(Permissions.RestoreContentToAlternateStack, request.user.email)) {
-      Future.successful(Forbidden(s"You do not have the ${Permissions.RestoreContentToAlternateStack.name} permission which is required to restore content from one stack to another"))
+  def restore(
+      sourceId: String,
+      contentId: String,
+      timestamp: String,
+      destinationId: String
+  ) = AuthAction.async { request =>
+    if (
+      !permissions.hasPermission(Permissions.RestoreContent, request.user.email)
+    ) {
+      Future.successful(
+        Forbidden(
+          s"You do not have the ${Permissions.RestoreContent.name} permission which is required to restore content"
+        )
+      )
+    } else if (
+      sourceId != destinationId && !permissions.hasPermission(
+        Permissions.RestoreContentToAlternateStack,
+        request.user.email
+      )
+    ) {
+      Future.successful(
+        Forbidden(
+          s"You do not have the ${Permissions.RestoreContentToAlternateStack.name} permission which is required to restore content from one stack to another"
+        )
+      )
     } else {
       val user = userFromPandaUser(request.user)
       val sourceStack = config.stackFromId(sourceId)
       val targetStack = config.stackFromId(destinationId)
       val snapshotId = SnapshotId(contentId, timestamp)
-      val result = snapshotApi.getSnapshot(sourceStack.snapshotBucket, snapshotId).flatMap[Result] {
-        case None => Attempt.Right(NotFound)
-        case Some(snapshot) => flexibleApi.restore(targetStack, user, contentId, snapshot).map(Ok(_))
-      }
+      val result = snapshotApi
+        .getSnapshot(sourceStack.snapshotBucket, snapshotId)
+        .flatMap[Result] {
+          case None           => Attempt.Right(NotFound)
+          case Some(snapshot) =>
+            flexibleApi
+              .restore(targetStack, user, contentId, snapshot)
+              .map(Ok(_))
+        }
       result.fold(
         errors => InternalServerError(s"Error whilst restoring: $errors"),
         identity
@@ -59,21 +87,35 @@ class Restore(
     // execution context under concurrent load and caused reachable stacks to be
     // spuriously reported as unavailable.
     val destinations = config.allStacks.map { stack =>
-      val destination = Destination(stack.id, stack.displayName, stack.stage, stack.stack,
-          stack.composerPrefix, stack.isSecondary, None, None, available = false)
+      val destination = Destination(
+        stack.id,
+        stack.displayName,
+        stack.stage,
+        stack.stack,
+        stack.composerPrefix,
+        stack.isSecondary,
+        None,
+        None,
+        available = false
+      )
 
       flexibleApi.changeDetails(stack, contentId).map { cdOption =>
         destination.withApiStatus(cdOption, available = true)
       } recover {
         // if we fail to talk to the stack's API for any reason then provide a destination with available set to false
         case NonFatal(e) =>
-          logger.warn(s"Couldn't communicate with Flexible stack at ${stack.apiPrefix}", e)
+          logger.warn(
+            s"Couldn't communicate with Flexible stack at ${stack.apiPrefix}",
+            e
+          )
           destination
       }
     }
     Future.sequence(destinations).map(d => Ok(Json.toJson(d)))
   }
 
-  protected val parser: BodyParser[AnyContent] = controllerComponents.parsers.default
-  protected val executionContext: ExecutionContext = controllerComponents.executionContext
+  protected val parser: BodyParser[AnyContent] =
+    controllerComponents.parsers.default
+  protected val executionContext: ExecutionContext =
+    controllerComponents.executionContext
 }
